a666ffc9bb51a1db6f7b5e1ef8f71257e63e9412
cinnoline <cinnoline10086@gmail.com>
2025-02-27 20:21:26 +0800

    update

--- routes/map.js
+++ routes/map.js
@@ -27,7 +27,7 @@ const colorMapping = {
 // ** Note: the function only makes the map display one type of data (marker/position) at a time.
 //    To display multiple types of data, it is necessary to reconstruct routers and transform this function to a middleware.
 function generateMapUrl(currentLocation, data, type, zoom = 16) {
-  const baseUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${currentLocation[0]},${currentLocation[1]}&zoom=${zoom}&size=400x400&maptype=roadmap`;
+  const baseUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${currentLocation[0]},${currentLocation[1]}&zoom=${zoom}&size=320x320&format=jpg-baseline&maptype=roadmap`;
   let markers = "";
   const customIconUrl = iconMapping[type];
   const currentLocationMarker = iconMapping["location"];
