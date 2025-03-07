c080ea681ea5b3cb4c7ebacc27b514d155d5deeb
cinnoline <cinnoline10086@gmail.com>
2025-03-07 19:03:14 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ import weatherRouter from "./weather.js";
 import facilitiesRouter from "./facilities.js";
 import transportRouter from "./transport.js";
 import locationRouter from "./location.js";
+import mapRouter from "./map.js";
 
 const app = express(); // create an express app
 const PORT = 8880; // set the port
@@ -14,6 +15,7 @@ app.use("/weather", weatherRouter); // use the weather router
 app.use("/facilities", facilitiesRouter); // use the facilities router
 app.use("/transport", transportRouter); // use the transport router
 app.use("/location", locationRouter); // use the location router
+app.use("/map", mapRouter); // use the location router
 
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`);
--- routes/map.js
+++ routes/map.js
@@ -1,9 +1,17 @@
 /** @format */
 
 import dotenv from "dotenv";
+import { Router } from "express";
+import express from "express";
+import axios from "axios";
 
 dotenv.config({ path: "../.env" });
 
+// create a router
+const router = Router();
+
+// use the express.json middleware
+router.use(express.json());
 // icon for each type of data, the Google Static Maps API requires the icon image to be accessible via a URL
 // ** Note: the icon images are stored in a DigitalOcean Spaces bucket
 const iconMapping = {
@@ -57,4 +65,54 @@ function generateMapUrl(currentLocation, data, type, zoom = 15) {
   return encodeURI(`${baseUrl}${markers}&language=en&key=${apiKey}`);
 }
 
-export default generateMapUrl;
+router.get("/", async (req, res) => {
+  try {
+    const { latitude, longitude } = req.query;
+    const currentLocation = [latitude, longitude];
+    const root = process.env.HOST;
+    const kmbResponse = await axios.get(
+      `${root}/transport/kmbStops/coordinates?lat=${latitude}&long=${longitude}`
+    );
+    console.log(kmbResponse);
+    // get the string
+    const kmbData = kmbResponse.data;
+    console.log(kmbData);
+    const baseUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${currentLocation[0]},${currentLocation[1]}&zoom=${zoom}&size=280x280&format=jpg-baseline&maptype=roadmap`;
+    let markers = "";
+    const customIconUrl = iconMapping[type];
+    const currentLocationMarker = iconMapping["location"];
+    // loop through the data array and add markers for each item
+    kmbData.forEach((item, index) => {
+      const color = colorMapping[type];
+      // if custom icon is provided, use it; otherwise, use the default marker
+      if (customIconUrl) {
+        markers += `&markers=icon:${customIconUrl}|${item.geometry[1]},${item.geometry[0]}`; // icon cannot be used with label or color
+      } else {
+        // distinguish the markers by color and label
+        markers += `&markers=color:${color}|label:${type
+          .charAt(0)
+          .toUpperCase()}|${item.geometry[1]},${item.geometry[0]}`;
+      }
+    });
+    // add a marker for the current location
+    if (currentLocation) {
+      markers += `&markers=icon:${currentLocationMarker}|${currentLocation[0]},${currentLocation[1]}`;
+    } else {
+      markers += `&markers=color:orange|label:L|${currentLocation[0]},${currentLocation[1]}`;
+    }
+
+    // add the API key to the URL
+    const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+
+    res
+      .status(200)
+      .send(encodeURI(`${baseUrl}${markers}&language=en&key=${apiKey}`));
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred");
+  }
+});
+
+// export default generateMapUrl;
+
+export default router;
